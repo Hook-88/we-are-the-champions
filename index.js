b2aab43430f339d6e1c8c1361ff8314d176a90fb
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2023-05-04 19:02:52 -0500

    Add logic to publish endorsements

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // javascript
+const publishBtn = document.getElementById('publish-btn')
+const endorsementInputField = document.getElementById('endorsement-input-field')
+const listOfEndorsements = document.getElementById('list-of-endorsements')
+
+publishBtn.addEventListener('click', publishEndorsement)
+
+function publishEndorsement() { 
+  const userEndorsement = endorsementInputField.value
+  clearEndorsementInputfield()
+  renderEndorsement(userEndorsement)
+}
+
+function clearEndorsementInputfield() {
+  endorsementInputField.value = ""
+}
+
+function renderEndorsement(endorsementMessage) {
+  const newListItem = document.createElement('li')
+  newListItem.textContent = endorsementMessage
+  listOfEndorsements.append(newListItem)
+}
+
