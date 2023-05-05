34910ad7d4aac08b7ea9829dd46bb5b8a0a472de
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2023-05-04 19:11:36 -0500

    Add logic to push endorsement to firebase

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 // javascript
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+  databaseURL: "https://playground-d4953-default-rtdb.firebaseio.com/"
+}
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const endorsementsInDB = ref(database, "endorsements")
+
 const publishBtn = document.getElementById('publish-btn')
 const endorsementInputField = document.getElementById('endorsement-input-field')
 const listOfEndorsements = document.getElementById('list-of-endorsements')
@@ -8,6 +18,8 @@ publishBtn.addEventListener('click', publishEndorsement)
 function publishEndorsement() { 
   const userEndorsement = endorsementInputField.value
   clearEndorsementInputfield()
+  //push to data base the value of userEdorsement
+  push(endorsementsInDB, userEndorsement)
   renderEndorsement(userEndorsement)
 }
 
